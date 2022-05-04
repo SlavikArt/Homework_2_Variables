78ac5289b86dfaaf758ed9e6a85e55821a4824ae
Vyacheslav <52282118+SlavikArt@users.noreply.github.com>
2022-05-04 16:38:00 +0300

    Update

---
+++ Variables_Switch_Without_Cell/README.md
@@ -0,0 +1,4 @@
+# Variables Switch Without Cell
+* [Variables_Switch_Without_Cell.cpp](Variables_Switch_Without_Cell.cpp)
+<p>Меняет значение 2-х переменных без вспомогательной</p>
+<img src="/images/Variables_Switch_Without_Cell.png">
---
+++ Variables_Switch_Without_Cell/Variables_Switch_Without_Cell.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "rus");
+
+    float varOne;
+    float varTwo;
+
+    cout << "Введите значение первой переменной: ";
+    cin >> varOne;
+
+    cout << "Введите значение второй переменной: ";
+    cin >> varTwo;
+
+                              // Пример: varOne = 10, varTwo = 35 
+    varOne += varTwo;         // varOne = 10 + 35 = 45
+    varTwo = varOne - varTwo; // varTwo = 45 - 35 = 10
+    varOne -= varTwo;         // varOne = 45 - 10 = 35
+                              // Вывод: varOne = 35, varTwo = 10
+
+    cout << "\nЗначение первой переменной: " << varOne << "\n";
+    cout << "Значение второй переменной: " << varTwo << "\n";
+}
